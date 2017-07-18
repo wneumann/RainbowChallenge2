3b7f895f555499746e1a8deae4de83b2676c3a8b
William D. Neumann <wdn@pax.aist.hon>
2017-07-18 08:39:20 -0500

    Initial Commit

---
+++ RainbowChallenge2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainbowChallenge2
+//
+//  Created by William D. Neumann on 7/18/17.
+//  Copyright © 2017 William D. Neumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
